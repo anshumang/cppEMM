76dc843b4faea24d9e5dacb01be053dda5400d0b
anshumang <goswami.anshuman@gmail.com>
2015-09-12 10:03:21 -0400

    # to /* */ for comments - R copy-paste error fixed

--- src/AllClasses.hpp
+++ src/AllClasses.hpp
@@ -1,29 +1,37 @@
-#######################################################################
-# cppEMM - Extensible Markov Model (EMM) for Data Stream Clustering in C++
-# Copyrigth (C) 2015 Michael Hahsler, Anshuman Goswami
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+/***********************************************************************
+* cppEMM - Extensible Markov Model (EMM) for Data Stream Clustering in C++
+* Copyrigth (C) 2015 Michael Hahsler, Anshuman Goswami
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
-#pragma once
+//#pragma once
+#ifndef _ALLCLASSES_HPP_
+#define _ALLCLASSES_HPP_
+
+#include <vector>
+typedef std::vector<int> numeric;
 
 class StreamClustering{
 
 };
 
 class tNN{
-
+  numeric threshold;
+  numeric lambda;
+  numeric lambda_factor;
 };
 
 class SimpleMC{
@@ -31,7 +39,8 @@ class SimpleMC{
 };
 
 class TRACDS{
-
+  numeric lambda;
+  numeric lambda_factor;
 };
 
 class EMMInherit : tNN, TRACDS{
@@ -42,3 +51,5 @@ class EMMAggregate {
  tNN m_tNN;
  TRACDS m_TRACDS; 
 };
+
+#endif
