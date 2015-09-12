d1fb2cd5366f8fb8c4ebb5fbd4275bc5bf2f6093
anshumang <goswami.anshuman@gmail.com>
2015-09-12 11:59:12 -0400

    Top level CMakeLists added, AllClasses.cpp with tNN CTOR added

--- src/AllClasses.cpp
+++ src/AllClasses.cpp
@@ -17,62 +17,11 @@
 * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-//#pragma once
-#ifndef _ALLCLASSES_HPP_
-#define _ALLCLASSES_HPP_
+#include "AllClasses.hpp"
 
-#include <vector>
-#include <set>
+tNN::tNN(numeric threshold, character measure, logical centroids, numeric lambda)
+ : m_measure(measure),
+   m_centroids(centroids),
+   m_lambda(lambda)
+{}
 
-typedef std::vector<int> numeric;
-typedef std::vector<char> character; //or use std::string??
-typedef std::vector<bool> logical;
-
-template<typename T>
-class environment
-{
-  typedef std::set<T> storage;
-};
-
-class StreamClustering
-{
-
-};
-
-//template<typename T>
-class tNN
-{
-  character m_measure;
-  logical m_centroids;
-  numeric m_threshold;
-  numeric m_lambda;
-  numeric m_lambda_factor;
-  //environment<int> tnn_d; 
-  tNN(numeric threshold, character measure, logical centroids, numeric lambda);
-};
-
-class SimpleMC
-{
-
-};
-
-class TRACDS
-{
-  numeric lambda;
-  numeric lambda_factor;
-};
-
-//class EMMInherit : tNN<int>, TRACDS
-class EMMInherit : tNN, TRACDS
-{
-
-};
-
-class EMMAggregate 
-{
- //tNN<int> m_tNN;
- tNN m_tNN;
- TRACDS m_TRACDS; 
-};
-
-#endif
